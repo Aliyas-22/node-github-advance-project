c9912b5dac5eada10913195c25b08f7ba67e29e3
Aliyas-22 <aliyafir09@gmail.com>
2026-03-11 20:38:07 +0530

    added:config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,26 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.node
+    }
+  },
+
+  {
+    files: ["tests/**/*.js"],
+    languageOptions: {
+      globals: globals.jest
+    }
+  },
+
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs"
+    }
+  }
 ]);
